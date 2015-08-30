383e06f61239524aae392e835988fbb741f6f367
Prelang Builder <builder@prelang.com>
2015-08-30 23:38:37 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Computador' to allow for create, update

--- app/admin/computador.rb
+++ app/admin/computador.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Computador do
+  permit_params :nombre, :ip, :mac
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
